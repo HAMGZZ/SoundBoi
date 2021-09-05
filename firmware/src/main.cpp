f76c739565dcb4a64b6780b8f1cc224a79d13683
Lewis Hamilton <vk2gzz@ieee.org>
2021-09-05 14:07:28 +1000

    Changed to ATTINY1614

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,34 +1,29 @@
 /*
- * Exhaust controller using hc-05 and elm327 module with an atmega328p
+ * SoundBoi Exahust controller attiny1614 to interface with BLAT!
+ * Version 1.0.0
  */
 
 #include <Arduino.h>
-#include "BluetoothSerial.h"
-#include "ESP32Servo.h"
+#include <SoftwareSerial.h>
 #include "ELMduino.h"
 
-#define DONGLE_NAME             "OBDII"     //Name of Bluetooth OBDII dongle
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
 
-#define DEBUG_PORT              Serial      //Serial port for debug (USB)
-#define ELM_PORT                SerialBT    //Serial port for bluetooth
-
-#define ONBOARD_LED             D9          //Onboard LED pin
-
-#define BUTTON                  D3          //Pin for button in
-#define MOTOR1                  D2          //Pin for motor 1 out
-#define MOTOR2                  D4          //Pin for motor 2 out
+#define BUTTON                  2          //Pin for button in
+#define OUTPUT_PIN              1          //PIN FOR OUTPUT HIGH = OPEN
 
 #define ENGINE_WARMUP_TIME      10          //Number of seconds before valves close after the engine starts
 
-BluetoothSerial SerialBT;
+#define IND_LED                 6
+#define CON_LED                 7
 
-Servo servo1;
-Servo servo2;
 
 ELM327 elm;
+SoftwareSerial debug(9, 10);
 
 int state = 0;
-bool connected = false;
 
 uint32_t rpm = 0;
 uint32_t speed = 0;
@@ -40,71 +35,66 @@ uint32_t engineOnTime = 0;
 uint32_t ledIndicatorTimer = 0;
 uint32_t blinkrate = 0;
 
+bool connected = false;
+
 bool rules();
-void open();
-void close();
 void buttonHandler();
 void blinkLed();
-int mapRange(int a1,int a2,int b1,int b2,int s);
+void open();
+void close();
 
-void setup()
+int main()
 {
-    DEBUG_PORT.begin(9600);
-    ELM_PORT.begin("ESP32", true);
-    DEBUG_PORT.println("Exhaust valve controlled... \n\rOpening valves....");
-    servo1.attach(MOTOR1);
-    servo2.attach(MOTOR2);
+    delay(1000);
+    Serial.begin(115200);
+    debug.begin(9600);
+    debug.printf("SoundBoi for BLAT! - Lewis Hamilton September 2021\r\n");
+    debug.printf("VERSION: %d.%d.%d", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
     pinMode(BUTTON, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(BUTTON), buttonHandler, FALLING);
     open();
-    DEBUG_PORT.println("Connecting to ELM327...");
-    if(!ELM_PORT.connect(DONGLE_NAME))
+    if(!elm.begin(Serial, true, 2000))
     {
-        DEBUG_PORT.println("Could not connect to ELM327 - Phase 1");
-        DEBUG_PORT.println("Entering manual mode...");
-        close();
         connected = false;
-    }
-    else if(!elm.begin(ELM_PORT, true, 2000))
-    {
-        DEBUG_PORT.println("Could not connect to ELM327 - Phase 2");
-        DEBUG_PORT.println("Entering manual mode...");
-        close();
-        connected = false;
+        debug.printf("COULD NOT CONNECT TO ELM327...");
+        digitalWrite(CON_LED, LOW);
     }
     else
     {
-        DEBUG_PORT.println("Connected!");
         connected = true;
+        debug.printf("CONNECTED TO ELM327");
+        digitalWrite(CON_LED, HIGH);
     }
-}
 
-void loop()
-{
-    delay(100);
-    float tempRPM = elm.rpm();
-    float tempSpeed = elm.kph();
-    float tempThrottle = elm.throttle();
-    float tempLoad = elm.engineLoad();
-    float tempCoolantTemp = elm.engineCoolantTemp();
-    float tempEngineOnTime = elm.runTime();
-
-
-    if(elm.status == ELM_SUCCESS)
+    while(true)
     {
-        rpm = (uint32_t)tempRPM;
-        speed = (uint32_t)tempSpeed;
-        throttle = (uint32_t)tempThrottle;
-        load = (uint32_t)tempLoad;
-        coolantTemp = (uint32_t)tempCoolantTemp;
-        engineOnTime = (uint32_t)tempEngineOnTime;
-        DEBUG_PORT.printf("RPM: %d\tSPEED: %d\t THROTTLE: %d\t LOAD: %d\t coolantTemp: %d\r", rpm, speed, throttle, load, coolantTemp);
-    }
+        if(connected)
+        {
+            delay(100);
+            float tempRPM = elm.rpm();
+            float tempSpeed = elm.kph();
+            float tempThrottle = elm.throttle();
+            float tempLoad = elm.engineLoad();
+            float tempCoolantTemp = elm.engineCoolantTemp();
+            float tempEngineOnTime = elm.runTime();
 
-    rules();
-    blinkLed();
-    ledIndicatorTimer++;
-    
+
+            if(elm.status == ELM_SUCCESS)
+            {
+                rpm = (uint32_t)tempRPM;
+                speed = (uint32_t)tempSpeed;
+                throttle = (uint32_t)tempThrottle;
+                load = (uint32_t)tempLoad;
+                coolantTemp = (uint32_t)tempCoolantTemp;
+                engineOnTime = (uint32_t)tempEngineOnTime;
+                debug.printf("RPM: %lf SPD: %lf THTL: %lf\% LOAD: %lf\% TEMP: %lfC ON-TIME: %lfs\r\n");
+            }
+
+            rules();
+            blinkLed();
+            ledIndicatorTimer++;
+        }
+    }
 }
 
 bool rules()
@@ -121,7 +111,6 @@ bool rules()
             close();
     }
 
-
     else if(state == 1 && engineOnTime > ENGINE_WARMUP_TIME)
     {
         if((rpm > 1900 || load > 30 || throttle > 30) && speed < 90)
@@ -136,25 +125,14 @@ bool rules()
             close();
     }
 
-
     else if(state == 2)
     {
         open();
     }
 
-
     else if(state == 3)
     {
         close();
-    }
-
-    
-    else if(state == 4 && engineOnTime > ENGINE_WARMUP_TIME)
-    {
-        int openVal = mapRange(0, 100, 0, 90, throttle);
-        servo1.write(openVal);
-        servo2.write(openVal);
-
     }
 
     if(rpm < 50 || engineOnTime < ENGINE_WARMUP_TIME)
@@ -166,14 +144,12 @@ bool rules()
 
 void open()
 {
-    servo1.write(0);
-    servo2.write(0);
+    digitalWrite(OUTPUT_PIN, HIGH);
 }
 
 void close()
 {
-    servo1.write(90);
-    servo2.write(90);
+    digitalWrite(OUTPUT_PIN, LOW);
 }
 
 void blinkLed()
@@ -184,40 +160,30 @@ void blinkLed()
         {
             blinkrate = 10;
             ledIndicatorTimer = 0;
-            digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+            digitalWrite(IND_LED, !digitalRead(IND_LED));
         }
         else if(state == 1)
         {
             blinkrate = 2;
             ledIndicatorTimer = 0;
-            digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+            digitalWrite(IND_LED, !digitalRead(IND_LED));
         }
         else if(state == 2)
         {
             blinkrate = 0;
             ledIndicatorTimer = 0;
-            digitalWrite(LED_BUILTIN, HIGH);
+            digitalWrite(IND_LED, HIGH);
         }
         else if(state == 3)
         {
             blinkrate = 0;
             ledIndicatorTimer = 0;
-            digitalWrite(LED_BUILTIN, LOW);
-        }
-        else if(state == 4)
-        {
-            blinkrate = 5;
-            ledIndicatorTimer = 0;
-            digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+            digitalWrite(IND_LED, LOW);
         }
 
     }
 }
 
-int mapRange(int a1,int a2,int b1,int b2,int s)
-{
-	return b1 + (s-a1)*(b2-b1)/(a2-a1);
-}
 
 void buttonHandler()
 {
@@ -227,7 +193,7 @@ void buttonHandler()
   if (interrupt_time - last_interrupt_time > 200) 
   {
       state++;
-      if(state > 4)
+      if(state > 3)
       {
           state = 0;
       }
